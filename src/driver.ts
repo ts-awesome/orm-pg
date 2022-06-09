55f9e2ea85a535a3046e5fd130b8978bd54113c8
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2022-06-09 15:14:00 +0300

    isolation level basis

--- src/driver.ts
+++ src/driver.ts
@@ -1,12 +1,12 @@
 import {Pool} from 'pg';
 
-import {IQueryDriver, ITransaction} from '@ts-awesome/orm';
+import {IQueryDriver, IsolationLevel, ITransaction} from '@ts-awesome/orm';
 import {PgExecutor} from './executor';
-import {PgTransaction} from './transaction';
+import {isSupportedIsolationLevel, PgTransaction, UNSUPPORTED_ISOLATION_LEVEL} from './transaction';
 import {injectable, unmanaged} from 'inversify';
 import { ISqlQuery } from './interfaces';
 
-const BEGIN = 'BEGIN';
+const BEGIN = 'BEGIN TRANSACTION';
 
 type PgTransactionalExecutor = Pick<Pool, 'query' | 'connect' | 'end'>;
 
@@ -19,9 +19,14 @@ export class PgDriver extends PgExecutor
     super(pool);
   }
 
-  public async begin(): Promise<ITransaction<ISqlQuery>> {
+  public async begin(isolationLevel?: IsolationLevel): Promise<ITransaction<ISqlQuery>> {
     const client = await this.pool.connect();
-    await client.query(BEGIN);
+
+    if (isolationLevel && !isSupportedIsolationLevel(isolationLevel as string)) {
+      throw new Error(UNSUPPORTED_ISOLATION_LEVEL + JSON.stringify(isolationLevel))
+    }
+
+    await client.query(BEGIN + (isSupportedIsolationLevel(isolationLevel) ? ' ISOLATION LEVEL ' + isolationLevel : ''));
     return new PgTransaction(client);
   }
 
--- src/transaction.ts
+++ src/transaction.ts
@@ -2,12 +2,14 @@ import {PoolClient} from 'pg';
 import {PgExecutor} from './executor';
 import {ISqlQuery} from './interfaces';
 import {injectable} from 'inversify';
-import {ITransaction} from '@ts-awesome/orm';
+import {IsolationLevel, ITransaction} from '@ts-awesome/orm';
 
 const COMMIT = 'COMMIT';
 const ROLLBACK = 'ROLLBACK';
+const SET_TRANSACTION_ISOLATION_LEVEL = 'SET TRANSACTION ISOLATION LEVEL';
 
 export const TRANSACTION_ALREADY_RESOLVED_ERROR = 'Transaction is already resolved.';
+export const UNSUPPORTED_ISOLATION_LEVEL = 'Unsupported isolation level ';
 
 export type PgTransactionalExecutorClient = Pick<PoolClient, 'query' | 'release'>;
 
@@ -25,6 +27,20 @@ export class PgTransaction extends PgExecutor implements ITransaction<ISqlQuery>
     return this.isFinished;
   }
 
+  // noinspection JSUnusedGlobalSymbols
+  public async setIsolationLevel(isolationLevel: IsolationLevel): Promise<void> {
+    if (this.finished) {
+      throw new Error(TRANSACTION_ALREADY_RESOLVED_ERROR);
+    }
+
+    if (!isSupportedIsolationLevel(isolationLevel as string)) {
+      throw new Error(UNSUPPORTED_ISOLATION_LEVEL + JSON.stringify(isolationLevel))
+    }
+
+    await this.conn.query(`${SET_TRANSACTION_ISOLATION_LEVEL} ${isolationLevel}`);
+  }
+
+  // noinspection JSUnusedGlobalSymbols
   public async commit(): Promise<void> {
     if (this.finished) {
       throw new Error(TRANSACTION_ALREADY_RESOLVED_ERROR);
@@ -38,6 +54,7 @@ export class PgTransaction extends PgExecutor implements ITransaction<ISqlQuery>
     }
   }
 
+  // noinspection JSUnusedGlobalSymbols
   public async rollback(): Promise<void> {
     if (this.finished) {
       throw new Error(TRANSACTION_ALREADY_RESOLVED_ERROR);
@@ -50,4 +67,15 @@ export class PgTransaction extends PgExecutor implements ITransaction<ISqlQuery>
       this.isFinished = true;
     }
   }
+}
+
+export function isSupportedIsolationLevel(isolationLevel: string): boolean {
+  switch (isolationLevel) {
+    case 'SERIALIZABLE':
+    case 'REPEATABLE READ':
+    case 'READ COMMITTED':
+    case 'READ UNCOMMITTED':
+      return true;
+  }
+  return false;
 }
